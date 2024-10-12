17216057fd136de60fb0b609bd54c5c48262440b
Wenrong1016 <wenrong5437@gmail.com>
2024-10-12 13:13:07 -0400

    Update amusment_park.py

--- assignment 2/amusment_park.py
+++ assignment 2/amusment_park.py
@@ -1,11 +1,13 @@
-age = 3
-if age < 18:
-    print("Admission is $25")
-elif age < 4:
+age = 105
+if age < 4:
     print("Admission is $0")
-elif age > 60:
+elif age < 18:
+    print("Admission is $25")
+elif age > 60 and age <= 100:
     print("Admission is $35")
 elif age > 100:
-    print("Admission is $0 and you get a free beer")
+    print("Admission is $10")
 else:
     print("Admission is $40")
+
+
